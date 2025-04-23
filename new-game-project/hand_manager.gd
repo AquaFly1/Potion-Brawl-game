1b379cc5b7f52121569d81fd3483a1d3fafb7c64
Louis Emery <emerylo@educ.college-montreal.qc.ca>
2025-04-23 16:00:34 -0400

    bah oui mec

--- new-game-project/hand_manager.gd
+++ new-game-project/hand_manager.gd
@@ -4,6 +4,8 @@ var hand_size = 3
 @export var hand: Array[Ingredient]
 @onready var card_path: Path2D = $Path2D
 @onready var container: HBoxContainer = $HBoxContainer
+@onready var discard: Area2D = $Area2D
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 
 var deck = []
 var card = preload("res://scenes/card.tscn")
@@ -29,13 +31,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if discard.has_overlapping_bodies():
+		print("b")
 
 func _on_discard_pressed() -> void:
 	
-	print("discard")
+	print("a")
 
 func draw_card(x: int):
 	for i in range(x):
 		hand.append(deck.pop_front())
-	print(hand)
+
+
+func _on_area_2d_body_entered(body: CardBase) -> void:
+	print("discard")
+	
--- new-game-project/scripts/card.gd
+++ new-game-project/scripts/card.gd
@@ -10,7 +10,6 @@ var target = Vector2(0,0)
 var movement = 0
 var previous_pos = Vector2.ZERO
 var speed = 0
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	previous_pos = global_position
@@ -23,27 +22,21 @@ func _gui_input(event: InputEvent) -> void:
 				drag_offset = get_global_mouse_position() - global_position
 			else:
 				is_grabbed = false
-				pivot_offset = Vector2(0,0)
+				#pivot_offset = Vector2(0,0)
 	elif event is InputEventMouseMotion and is_grabbed:
 		var tween = get_tree().create_tween()
 		tween.tween_property(self, "global_position", get_global_mouse_position()- drag_offset, 0.1)
-		pivot_offset = get_global_mouse_position()
-		tween.set_ease(Tween.EASE_OUT)
+		#pivot_offset = get_global_mouse_position()
 		
 
 func _process(delta: float) -> void:
 	movement = global_position - previous_pos
-	var distance = global_position.distance_to(previous_pos)
-	speed = distance/delta
+	speed = movement.x/delta
 	previous_pos = global_position
 	var tween = get_tree().create_tween()
 	if not is_grabbed:
-		
-		tween.tween_property(self, "position", target, 1)
-	if movement.x > 0:
-		rotation_degrees = speed/100
-	elif movement.x < 0:
-		rotation_degrees = -speed/100
+		tween.tween_property(self, "position", target, 0.5)
+	rotation_degrees = speed/100
 
 func play_card():
 	pass
