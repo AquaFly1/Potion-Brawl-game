5eab777836264190f1d197ce1e241a0b31c7da38
Louis Emery <emerylo@educ.college-montreal.qc.ca>
2025-04-30 15:44:47 -0400

    xzc

--- new-game-project/hand_manager.gd
+++ new-game-project/hand_manager.gd
@@ -5,6 +5,7 @@ var hand_size = 3
 @onready var card_path: Path2D = $Path2D
 @onready var container: HBoxContainer = $HBoxContainer
 @onready var discard: Button = $Discard
+@onready var area_2d: Area2D = $Discard/Area2D
 
 var deck = []
 var card = preload("res://scenes/card.tscn")
@@ -15,25 +16,28 @@ func _ready() -> void:
 	deck = Deck.shuffle_deck(Deck.full_deck)
 	draw_card(hand_size)
 	Deck.card_released.connect(release_card)
+	
+#		card_image.rotation = card_follow.rotation
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	for i in range(hand.size()):
 		var card_follow = PathFollow2D.new()
 		var card_image = card.instantiate()
 		card_path.add_child(card_follow)
 		card_follow.add_child(card_image)
 		card_image.load_card(hand[i])
-		card_follow.progress_ratio = float(i)/float(hand_size - 1)
+		card_follow.progress_ratio = float(i)/float(hand.size() - 1)
 		card_image.target = card_follow.position
-#		card_image.rotation = card_follow.rotation
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		
+		
 
 func release_card(card: CardBase):
 	if mouse_on_discard:
-		if discard.has_overlapping_bodies():
-			discard_pile.append(hand.pop_at(discard.get_overlapping_bodies()))
-			print(discard_pile)
+		discard_pile.append(hand.pop_at(0))
+		hand.pop_at(0)
+		draw_card(1)
+		print(discard_pile)
 
 func draw_card(x: int):
 	for i in range(x):
@@ -41,8 +45,7 @@ func draw_card(x: int):
 
 func _on_discard_mouse_entered() -> void:
 	mouse_on_discard = true
-	print("B")
 
 func _on_discard_mouse_exited() -> void:
 #	mouse_on_discard = false
-	print("A")
+	pass
